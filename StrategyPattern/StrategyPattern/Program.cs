991f5933d46fc8123a5bffd4351fe7f38bae2864
Alex Carlton <ahcarlton@gmail.com>
2021-03-07 16:14:41 -0500

    Added item order kata to refactor

--- StrategyPattern/StrategyPattern/Program.cs
+++ StrategyPattern/StrategyPattern/Program.cs
@@ -1,12 +1,26 @@
-﻿using System;
+﻿using StrategyPattern.Business.Models;
+using System;
 
 namespace StrategyPattern
 {
-    class Program
+    internal static class Program
     {
-        static void Main(string[] args)
+        private static void Main()
         {
-            Console.WriteLine("Hello World!");
+            var order = new Order
+            {
+                ShippingDetails = new ShippingDetails
+                {
+                    OriginCountry = "Sweden",
+                    DestinationCountry = "Sweden"
+                }
+            };
+
+            order.LineItems.Add(new Item("CSHARP_SMORGASBORD", "C# Smorgasbord", 100m, ItemType.Literature), 1);
+            order.LineItems.Add(new Item("CONSULTING", "Building a website", 100m, ItemType.Service), 1);
+
+            Console.WriteLine(order.GetTax());
+            Console.ReadKey();
         }
     }
 }
---
+++ StrategyPattern/StrategyPattern/README.md
@@ -0,0 +1,7 @@
+# Strategy Pattern
+
+The strategy pattern (also known as the policy pattern) is a behavioral software design pattern that enables selecting an algorithm at runtime. Instead of implementing a single algorithm directly, code receives run-time instructions as to which in a family of algorithms to use
+
+## Code Kata
+
+The idea here is that we are doing order processing. We have this sales system that will process an order. What we want to do is to calculate the tax on our particular order. Calculating the tax is different depending on where you live in the world.
